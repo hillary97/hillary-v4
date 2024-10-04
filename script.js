29a43b0f2fe67e182bd2ab2bac5728c785ad6753
HILLARY  NTHOME <58300609+hillary97@users.noreply.github.com>
2024-10-04 19:59:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+		function front(){
+
+			
+			var conte=document.getElementById('contentsection').innerHTML="<p >Html, css, boostrap, react,javascript</p>";
+		}
+		function back(){
+			var conte=document.getElementById('contentsection').innerHTML="<p> PHP, Laravel, Node Js, MySql, Python, Sql, Java </p>";
+		}
+		function db(){
+			
+			var conte=document.getElementById('contentsection').innerHTML="<p> Maria DB, MySql, Sql </p>";
+		}function graphics(){
+			
+			var conte=document.getElementById('contentsection').innerHTML="<p>Figma, Canva, Adobe Photoshop, Illustrator, CoreDraw</p>";
+		}
+		function vc(){
+			
+			var conte=document.getElementById('contentsection').innerHTML="<p>GIT, Github</p>";
+		}
+	
